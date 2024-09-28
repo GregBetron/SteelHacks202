f191f619870717e757dea888c406a6a729e6540f
Greg Betron <gregb4789@gmail.com>
2024-09-28 11:05:00 -0400

    Initial Commit

---
+++ Hackathon/HackathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackathonApp.swift
+//  Hackathon
+//
+//  Created by Greg Betron on 9/28/24.
+//
+
+import SwiftUI
+
+@main
+struct HackathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
