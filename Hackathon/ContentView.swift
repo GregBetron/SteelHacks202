48a39a97f726655fc40149ae50b4c0d7cc8881e5
HaidenH <haidenhunter05@gmail.com>
2024-09-28 13:36:30 -0400

    podfile

--- Hackathon/ContentView.swift
+++ Hackathon/ContentView.swift
@@ -11,8 +11,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        
+                
         // Push-Up Function
+        Button {
+            print("do yo push ups yall!!!")
+        } label : {
+            Label("Push-Ups", systemImage:"figure.wrestling")
+                .padding()
+                .frame(height:125)
+                .frame(width:350)
+                .font(.system(size:50))
+                .foregroundStyle(.white)
+                .background(.blue)
+                .buttonStyle(.bordered)
+                .controlSize(.extraLarge)
+                .cornerRadius(20)
+        }
+
+        // Other Function
+        Button {
+            print("do yo push ups yall!!!")
+        } label : {
+            Label("Push-Ups", systemImage:"figure.wrestling")
+                .padding()
+                .frame(height:125)
+                .frame(width:350)
+                .font(.system(size:50))
+                .foregroundStyle(.white)
+                .background(.blue)
+                .buttonStyle(.bordered)
+                .controlSize(.extraLarge)
+                .cornerRadius(20)
+        }
+  
+        // Other Function
         Button {
             print("do yo push ups yall!!!")
         } label : {
